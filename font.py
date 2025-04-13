140d02ee6eac6f2d147e6b5bf7cc9b1836e05abd
HisayukiNomura <hnomura@fa2.so-net.ne.jp>
2025-04-13 17:48:40 +0900

    udpate. almost working

--- font.py
+++ font.py
@@ -3,9 +3,13 @@ from fontTools.ttLib import TTFont
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import codecs
+import argparse
+from pathlib import Path
+
 
 ## フォントをビットマップにレンダリングする関数
 def render_glyph_to_bitmap(font, char, font_size=64,X_offset = 0,Y_offset=-1):
+    global isImage
     # FontToolsでフォントファイルを解析
     #ttfont = TTFont(font_path)
     #print(f"Font Family: {ttfont['name'].getDebugName(1)}")  # フォントファミリー名を取得
@@ -18,7 +22,10 @@ def render_glyph_to_bitmap(font, char, font_size=64,X_offset = 0,Y_offset=-1):
     image = Image.new("L", image_size, color=255)  # グレースケール画像（白背景）
     draw = ImageDraw.Draw(image)
     draw.text((X_offset, Y_offset), char, font=font, fill=1)  # 文字を黒で描画
-
+    if isImage:
+        image.show()
+        print("Displaying the image. Please close the window before pressing [ENTER]...")
+        input()  
     return image
 
 ## 画像データを、ビットマップ配列にする
@@ -128,29 +135,64 @@ def printBitArray(bitArray):
         print() 
     print()
 
-"""
-struct KanjiData {
-    uint32_t Unidoe;
-    uint16_t SJIS;
-    uint16_t JIS;
-    uint8_t width;
-    uint8_t height;
-    uint16_t offsetBMP;
-};
-static const KanjiData fontNameData[] = { {unicode,sjis,jis,12,12,0x0001}, ,,,}
 
-static const uint8_t fontNameBitmap[] = {....}
-という感じにして生きた。
 
-問題は、offsetBMP。レンダリングするまでサイズは正確にはわからない。
-リストの各要素はタプルになっているので、サイズの追加が面倒
-　⇒　別のリストを用意するか？　タプルを書き換えるか？そもそもタプルを止めて、リストのリストにするか？
-"""
 
 if __name__ == "__main__":
-    font_path = "E:\Programing\VSCode\Font\JF-Dot-Shinonome12.ttf"  # フォントファイルのパス
+    parser = argparse.ArgumentParser(description="Convert Bitmap font from TrueType(.TTF) file.",
+                                        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    epilog='''
+    Additional helpful information can be added here.
+    '''
+    )
+    parser.add_argument("font_path", type=str,help="Path to the turetype font file")
+    parser.add_argument("-n", "--name", type=str, default="", help="Name of generated structure instance")
+    parser.add_argument("-s", "--size", type=int, default=12, help="Font size")
+    parser.add_argument("-xo", "--xoffset", type=int, default=0, help="X offset")
+    parser.add_argument("-yo", "--yoffset", type=int, default=-1, help="Y offset")
+    parser.add_argument("-cs", "--codeset", choices=["ALL","LEVEL1","JYOUYOU","KYOUIKU","TEST"], default="ALL", help="Code set to use.")
+    parser.add_argument("-o", "--output", type=str, default="XXX.XXX", help="Output file name")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output")
+    parser.add_argument("-i", "--image", action="store_true", help="Display truetype font image")
+    args = parser.parse_args()
+    font_path = args.font_path
+    font_size = args.size
+    x_offset = args.xoffset
+    y_offset = args.yoffset
+    code_set = args.codeset
+    output_file = args.output
+    isVerbose = args.verbose
+    isImage = args.image
+
+
+
+
+    #入力ファイルが　”test" なら、テスト用のデータを読み込む
+    if (font_path == "test"):
+        font_path = "E:\Programing\VSCode\Font\JF-Dot-Shinonome12.ttf"
+
+    #出力ファイルが指定されなかったら、フォントファイル名に基づいて、.hファイルを作成する
+    if (output_file == "XXX.XXX"):
+        output_file = font_path.split(".")[0]+ "_" + str(font_size).zfill(2)  + "_" + code_set +".h"
+
+    if (args.name == ""):
+        namebase = Path(font_path).name.split(".")[0]
+        # 置換対象の文字リスト
+        invalid_chars = {"+", "-", "*" ,"/", " ", " ", "(", ")", "[", "]", "{", "}", ":", ";", ",", ".", "<", ">", "?", "!", "@", "#", "$", "%", "^", "&", "~", "`"}
+        # 置換後の文字列を作成
+        cleaned_name = "".join(char for char in namebase if char not in invalid_chars)
+
+
+
+        structure_name =  cleaned_name+ "_" + str(font_size).zfill(2)  + "_" + code_set
+        bitmapdata_name = cleaned_name+ "_" + str(font_size).zfill(2)  + "_" + code_set + "_bitmap"
+    else:
+        structure_name = args.name
+        bitmapdata_name =  args.name+"_bitmap"
+
+    #font_path = "E:\Programing\VSCode\Font\JF-Dot-Shinonome12.ttf"  # フォントファイルのパス
     #char = "野"  # レンダリングする文字
-    font_size = 12  # フォントサイズを指定
+    #font_size = 12  # フォントサイズを指定
     
     # Pillowでフォントを読み込み
     font =  ImageFont.truetype(font_path, font_size)
@@ -176,7 +218,7 @@ if __name__ == "__main__":
     
 
 
-    codeList , bmpList = convToDataAndBitmap(codeList,font_size,0,-1)
+    codeList , bmpList = convToDataAndBitmap(codeList,font_size,x_offset,y_offset)
 
     """
     for code in codeList:
@@ -185,154 +227,80 @@ if __name__ == "__main__":
         printBitArray(bmpList[code[6]])
     """
     
-    #まず、ビットマップ情報の表示
-    print ("struct KanjiData {")
-    print ("\tuint32_t Unidoe;")
-    print ("\tuint16_t SJIS;")
-    print ("\tuint16_t JIS;")
-    print ("\tuint8_t width;")
-    print ("\tuint8_t height;")
-    print ("\tuint32_t offsetBMP;")
-    print ("};")
-    print()
-    print ("static const KanjiData fontNameData[] = {")
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write("// This file is auto generated by font.py\n")
+        f.write("// Do not edit this file directly.\n")
+        f.write("// Font: " + Path(font_path).name + "\n")
+        f.write("// Font Size: " + str(font_size) + "\n")
+        f.write("// Code Set: " + code_set + "\n")
+        
 
-    for i, code in enumerate(codeList):
-        strWkLine = ""
-        strWkLine = f"\t{{ 0x{hex(code[0])[2:].zfill(8)} , {hex(code[1])} , {hex(code[2])} , {code[4]} ,{code[5]} , 0x{hex(code[6])[2:].zfill(8)} }}}}"
-        if i < len(codeList) - 1:
-            strWkLine += ","
-        else:
-            strWkLine += " "
-        strWkLine += f"\t// {code[3]}\r\n"
-        print(strWkLine,end="")
-    print("};")
-    print()
+        #まず、ビットマップ情報の表示
+        strOutput = ""
+        
+        strOutput += "struct KanjiData {\n"
+        strOutput += "\tuint32_t Unidoe;\n"
+        strOutput += "\tuint16_t SJIS;\n"
+        strOutput += "\tuint16_t JIS;\n"  
+        strOutput += "\tuint8_t width;\n"
+        strOutput += "\tuint8_t height;\n"
+        strOutput += "\tuint32_t offsetBMP;\n"
+        strOutput += "};\n"
 
-    # 次に対応するビットマップデータを表示
-    offset = 0
-    print("static const uint8_t fontNameBitmap[] = {")
-    for charNo , row in enumerate(bmpList):
-        oneBlock = ""
-        oneBlock += f"// UNICODE:0x{hex(codeList[charNo][0])[2:].zfill(8)} -  Offset:0x{hex(codeList[charNo][6])[2:].zfill(8)}   -- CHAR:\"{codeList[charNo][3]}\" \r\n"
-        for value in row:
-            bitsInfo = ""
-            for x , xdata in enumerate(value) :
-                bitsInfo += bin(xdata)[2:].zfill(8)
-                oneBlock += f"0x{hex(xdata)[2:].zfill(2)}"
+        f.write(strOutput)
+        if isVerbose:
+            print(strOutput,end="");
+            print()
 
-                #print(f"0x{hex(xdata)[2:].zfill(2)}",end = "")
-                if x < len(value) - 1:
-                    oneBlock += ","
-                else:
-                    oneBlock += ","
-                    oneBlock += f"\t\t// {bitsInfo} \r\n"
-                    #print(f"\t\t// {bitsInfo}")
-        oneBlock += "\r\n"
-        print (oneBlock,end="")
-        #print("")
-    print ("};")    
-    print()
+        strOutput = ""
+        strOutput += f"static const KanjiData {structure_name}[] = {{\n"
+        for i, code in enumerate(codeList):
+            strWkLine = ""
+            strWkLine = f"\t{{ 0x{hex(code[0])[2:].zfill(8)} , {hex(code[1])} , {hex(code[2])} , {code[4]} ,{code[5]} , 0x{hex(code[6])[2:].zfill(8)} }}}}"
+            if i < len(codeList) - 1:
+                strWkLine += ","
+            else:
+                strWkLine += " "
+            strWkLine += f"\t// {code[3]}\n"
+            strOutput += strWkLine
+        strOutput += "};\n"
 
-"""
-truct KanjiData {
-    uint32_t Unidoe;
-    uint16_t SJIS;
-    uint16_t JIS;
-    uint8_t width;
-    uint8_t height;
-    uint16_t offsetBMP;
-};
-static const KanjiData fontNameData[] = { {unicode,sjis,jis,12,12,0x0001}, ,,,}
+        f.write(strOutput)
+        if isVerbose:
+            print (strOutput,end="")
 
-static const uint8_t fontNameBitmap[] = {....}
-という感じにして生きた。            
-"""
-"""
-    for code in codeJISL1:
-        codeJIS , codeSJIS , codeUTF8 = jis_to_encodings(code)
-        if codeJIS == 0 or codeSJIS == 0 or codeUTF8 == 0:
-            print(f"JISコード: {hex(code)}をスキップ")
-            continue
-        #print(f"JISコード: {hex(codeJIS)}, Shift-JIS: {hex(codeSJIS)}, UTF-8: {hex(codeUTF8)} ")
-
-        utf8_bytes = codeUTF8.to_bytes(3, byteorder='big')
-        unicode_char = utf8_bytes.decode('utf-8')
-        print(f"JISコード: {hex(codeJIS)}, Shift-JIS: {hex(codeSJIS)}, UTF-8: {hex(codeUTF8)} : 文字：{unicode_char}")
-        CodeList.append(codeUTF8,codeSJIS,codeJIS,unicode_char)
-
-        # char = unicode_char  # レンダリングする文字
-        #
-        #bitmap_image = render_glyph_to_bitmap(font, char, font_size)
-        #display_bitmap_data(bitmap_image)
+        # 次に対応するビットマップデータを表示
+        offset = 0
+        strOutput = f"static const uint8_t {bitmapdata_name}[] = {{\n"
+        f.write(strOutput)
+        if isVerbose:
+            print (strOutput,end="")
 
 
-    bitmap_image = render_glyph_to_bitmap(font, char, font_size)
-    # bitmap_image.show()  # ビットマップ画像を表示
-    display_bitmap_data(bitmap_image)
-"""
+        for charNo , row in enumerate(bmpList):
+            oneBlock = ""
+            oneBlock += f"// UNICODE:0x{hex(codeList[charNo][0])[2:].zfill(8)} -  Offset:0x{hex(codeList[charNo][6])[2:].zfill(8)}   -- CHAR:\"{codeList[charNo][3]}\" \n"
+            for value in row:
+                bitsInfo = ""
+                for x , xdata in enumerate(value) :
+                    bitsInfo += bin(xdata)[2:].zfill(8)
+                    oneBlock += f"\t0x{hex(xdata)[2:].zfill(2)}"
 
-"""
-import codecs
+                    #print(f"0x{hex(xdata)[2:].zfill(2)}",end = "")
+                    if x < len(value) - 1:
+                        oneBlock += ","
+                    else:
+                        oneBlock += ","
+                        oneBlock += f"\t\t// {bitsInfo} \n"
+                        #print(f"\t\t// {bitsInfo}")
+            oneBlock += "\n"
+            f.write(oneBlock)
+            if isVerbose:
+                print (oneBlock,end="")
+        f.write("};\n")
+        if isVerbose:
+            print ("};")
+        print()
 
-jis_code = 0x3022  # JISコード
-try:
-    # 2バイトのJISコードをバイト列に変換
-    jis_bytes = jis_code.to_bytes(2, byteorder='big')
-    hex_representation = jis_bytes.hex()
-    print(f"JISコードのバイト列: {hex_representation}")
-    
-    
-    # JISコードをデコードしてUnicode文字に変換
-    unicode_char = codecs.decode(jis_bytes, 'iso2022_jp')
-    print(f"対応する文字: {unicode_char}")
+        print(f"Output file: {output_file} created.")
 
-    # Unicode文字をUTF-8バイト列にエンコード
-    utf8_bytes = unicode_char.encode('utf-8')
-    hex_representation = utf8_bytes.hex()
-    print(f"UTF-8バイト列: {hex_representation}")
-
-except Exception as e:
-    print(f"エラー: {e}")
-"""
-
-
-"""
-import freetype
-import numpy as np
-import matplotlib.pyplot as plt
-
-def render_glyph_from_ttc(ttc_path, font_index, char, font_size=64):
-    # フォントコレクションから特定のフォントを選択
-    face = freetype.Face(ttc_path, font_index)
-    face.set_char_size(font_size * 64)
-
-    # 指定した文字をロード
-    face.load_char(char)
-    bitmap = face.glyph.bitmap
-
-    # ビットマップデータを取得
-    rows, width = max(bitmap.rows, 1), max(bitmap.width, 1)  # 幅と高さが0にならないように最低値を1に設定
-    bitmap_data = np.zeros((rows, width), dtype=np.uint8)
-
-    if bitmap.buffer:
-        for row in range(bitmap.rows):
-            bitmap_data[row, :bitmap.width] = bitmap.buffer[row * bitmap.width: (row + 1) * bitmap.width]
-
-    return bitmap_data
-
-def display_bitmap(bitmap):
-    plt.imshow(bitmap, cmap="gray", interpolation="nearest")
-    plt.axis("off")
-    plt.show()
-
-if __name__ == "__main__":
-    ttc_path = "E:\Programing\VSCode\Font\JF-Dot-Shinonome12.ttf"  # TTCファイルのパス
-    font_index = 0  # 使用したいフォントのインデックス（0から始まる）
-    char = "あ"  # レンダリングする文字
-    font_size = 12  # フォントサイズ
-
-    bitmap = render_glyph_from_ttc(ttc_path, font_index, char, font_size)
-    display_bitmap(bitmap)
-
-"""
